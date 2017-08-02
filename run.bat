67e59564eec0f1f47af74586cd68d948fed781d5
Ian <ian.vink@outlook.com>
2017-08-02 20:06:45 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ title node JS script
 echo Starting
 echo ------------------------------
 color 0a
-node main.js -port 80 -rpath /siteFiles -dirList true -startpage 2 index.html index.php
+node webserver.js -port 80 -rpath /siteFiles -dirList true -startpage 2 index.html index.php
 color 08
 echo ------------------------------
 pause
